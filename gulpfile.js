3b564978d1d2b8ff0f26140a08d47ce6c0ab6bfc
David Raines <davidraines.ny@gmail.com>
2016-11-26 10:43:18 -0500

    added coffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
     
-    gutil.log('workflows are special');
-
+    gulp.src(coffeeSources)
+      .pipe(coffee({ bare: true })
+ 		.on('error', gutil.log))
+      	  .pipe(gulp.dest('components/scripts'))
 });
 
 
