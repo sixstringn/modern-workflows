942a814f998ad6260832f0ddf7f36454e3b631f3
David Raines <davidraines.ny@gmail.com>
2016-11-26 11:06:42 -0500

    added concatenated JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/coffee/pixgrid.coffee',
+	'components/coffee/rclick.coffee',
+	'components/coffee/tagline.coffee',
+	'components/coffee/template.coffee'
+];
 
-gulp.task('coffee', function() {
-    
+gulp.task('coffee', function() { 
     gulp.src(coffeeSources)
       .pipe(coffee({ bare: true })
  		.on('error', gutil.log))
       	  .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() { 
+    gulp.src(jsSources)
+      .pipe(concat('script.js'))
+      .pipe(gulp.dest('builds/development/js'))
 });
 
 
